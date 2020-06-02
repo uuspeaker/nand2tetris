6e69c12c0d8f29ae20626fe83c8dfab73a387d79
tianhan <63253294@qq.com>
2020-06-02 12:29:16 +0800

    重构，使pop翻译不需要使用temp变量

--- 08/VMTranslator/PushInstruction.py
+++ 08/VMTranslator/PushInstruction.py
@@ -49,40 +49,50 @@ class PushInstruction:
         code = []
         address_name = self.param_dic.get(param_type, '')
         # code.append('//pop {} {}'.format(param_type, offset))
-        # 获取当前stack顶端的值
+        # 获取当前stack顶端的值，保存到D寄存器
         code.append('@SP')
         code.append('A=M-1')
         code.append('D=M')
-        # 把值存储到临时地址R11
-        code.append('@R11')
-        code.append('M=D')
+        # 把值存储到临时地址stack_top_value
+        # code.append('@stack_top_value')
+        # code.append('M=D')
         # 找到要存储区段的地址，将地址存到临时地址R12
         if param_type == 'temp':
             address = int(offset) + 5
             code.append('@{}'.format(address))
-            code.append('D=A')
+            code.append('M=D')
         elif param_type == 'static':
             code.append('@Foo.{}'.format(offset))
-            code.append('D=A')
+            code.append('M=D')
         elif param_type == 'pointer' and offset == '0':
             code.append('@R3')
-            code.append('D=A')
+            code.append('M=D')
         elif param_type == 'pointer' and offset == '1':
             code.append('@R4')
-            code.append('D=A')
+            code.append('M=D')
         else:
+            # 找到要存储区段的地址，将地址存到临时地址temp_address
             code.append('@{}'.format(offset))
             code.append('D=A')
             code.append('@{}'.format(address_name))
             code.append('D=D+M')
-        code.append('@R12')
-        code.append('M=D')
-        # 将此临时值R11存到区段地址R12
-        code.append('@R11')
-        code.append('D=M')
-        code.append('@R12')
-        code.append('A=M')
-        code.append('M=D')
+            code.append('@temp_address')
+            code.append('M=D')
+            # 获取当前stack顶端的值，保存到temp_address
+            code.append('@SP')
+            code.append('A=M-1')
+            code.append('D=M')
+            code.append('@temp_address')
+            code.append('A=M')
+            code.append('M=D')
+        # code.append('@R12')
+        # code.append('M=D')
+        # 将此临时值stack_top_value存到区段地址R12
+        # code.append('@stack_top_value')
+        # code.append('D=M')
+        # code.append('@R12')
+        # code.append('A=M')
+        # code.append('M=D')
         # 当前stack指向前一个地址
         code.append('@SP')
         code.append('M=M-1')
