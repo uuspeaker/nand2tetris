a1376e523d1201214137ed684dd3c729bff68812
tianhan <63253294@qq.com>
2020-06-07 16:59:31 +0800

    完成变量定义翻译

--- 10/Parser/CompilationEngine.py
+++ 10/Parser/CompilationEngine.py
@@ -10,33 +10,27 @@ class CompilationEngine:
     def compile_class(self):
         # 开头必须为class className {的格式
         self.write('<class>')
-        self.has_value('class')
-        self.eat_token()
+        self.eat('class')
         self.has_name('identifier')
-        self.eat_token()
-        self.has_value('{')
-        self.eat_token()
+        self.eat_current()
+        self.eat('{')
         # 下面的内容必须为0个或多个classVarDex, subroutineDec
         while (True):
             if self.is_value(['static', 'field']):
-                self.compile_class_var_dec()
+                self.eat(['static', 'field'])
+                self.compile_var_dec()
             elif self.is_value(['constructor', 'function', 'method']):
                 self.compile_subroutine_dec()
             else:
                 # 进入此处表示class解析完成，必须以}结束
                 break
         # 最后必须以}结尾
-        self.has_value('}')
+        self.eat('}')
         self.write('</class>')
         return self.new_tokens
 
     def write(self, value):
         self.new_tokens.append(value)
-
-    def eat_token(self):
-        self.new_tokens.append(self.get_current_token())
-        print('翻译完成:{}'.format(self.get_current_token()))
-        self.token_indx += 1
 
     def get_current_token(self):
         return self.old_tokens[self.token_indx]
@@ -60,13 +54,21 @@ class CompilationEngine:
         if not self.is_name(name_range):
             self.raise_exception('节点名称必须为 {}'.format(name_range))
 
-    def has_value(self, value_range):
+    # 处理当前token并进入下一个token
+    # 如果传入value_range则检查当前token必须在传入值范围内
+    def eat(self, value_range):
         if not self.is_value(value_range):
-            self.raise_exception('节点值必须为 {}'.format(value_range))
+            self.raise_exception('token值必须为 {}'.format(value_range))
+        self.eat_current()
+
+    def eat_current(self):
+        self.new_tokens.append(self.get_current_token())
+        print('翻译完成:{}'.format(self.get_current_token()))
+        self.token_indx += 1
 
     def has_name_value(self, name_range, value_range):
         self.has_name(name_range)
-        self.has_value(value_range)
+        self.eat(value_range)
 
     def get_current_token_name(self):
         return self.get_token_content(r'<(\w+)>')
@@ -89,7 +91,19 @@ class CompilationEngine:
         return
 
     def compile_statements(self):
-        return
+        print('============')
+        self.new_tokens.append('<statements>')
+        while True:
+            print('------------------')
+            if self.is_value('if'):
+                self.compile_if_statement()
+            elif self.is_value('while'):
+                self.compile_while_statement()
+            elif self.is_value('let'):
+                self.compile_let_statement()
+            else:
+                break
+        self.new_tokens.append('</statements>')
 
     def compile_if_statement(self):
         return
@@ -98,38 +112,46 @@ class CompilationEngine:
         return
 
     def compile_let_statement(self):
-        return
-
-    def compile_class_var_dec(self):
-        self.next_token_value in ('static', 'field', 'method')
-        return
+        self.new_tokens.append('<letStatement>')
+        self.eat('let')
+        self.has_name('identifier')
+        
+        self.eat(')')
+        self.new_tokens.append('</letStatement>')
 
     def compile_subroutine_dec(self):
         self.new_tokens.append('<subroutineDec>')
-        self.eat_token()
+        self.eat(['constructor', 'function', 'method'])
         if (not self.is_value(['void', 'int', 'char', 'boolean'])) and (not self.is_name('identifier')):
             self.raise_exception('修饰符必须为 void, int, char, boolean, className')
-        self.eat_token()
+        self.eat_current()
         self.has_name('identifier')
-        self.eat_token()
+        self.eat_current()
         self.compile_parameter_list()
         self.compile_subroutine_body()
         self.new_tokens.append('</subroutineDec>')
 
     def compile_subroutine_body(self):
         self.new_tokens.append('<subroutineBody>')
-        self.has_value('{')
-        self.eat_token()
-        self.new_tokens.append('<parameterList>')
-        self.compile_var_dec()
-        self.compile_statements()
-        self.has_value('}')
-        self.eat_token()
+        self.eat('{')
+
+        while (True):
+            print('++++++++++++')
+            if self.is_value('var'):
+                self.new_tokens.append('<varDec>')
+                self.eat('var')
+                self.compile_var_dec()
+                self.new_tokens.append('</varDec>')
+            elif self.is_value(['if', 'while', 'let']):
+                self.compile_statements()
+            else:
+                break
+
+        self.eat('}')
         self.new_tokens.append('</subroutineBody>')
 
     def compile_parameter_list(self):
-        self.has_value('(')
-        self.eat_token()
+        self.eat('(')
         self.new_tokens.append('<parameterList>')
         index = 0
         while (True):
@@ -138,22 +160,29 @@ class CompilationEngine:
                 break
             if self.is_value(','):
                 # 如果是逗号，表示后面还有参数
-                self.eat_token()
                 self.compile_type()
             else:
                 self.compile_type()
         self.new_tokens.append('</parameterList>')
-        self.has_value(')')
-        self.eat_token()
+        self.eat(')')
 
+    # 格式必须为type varName
     def compile_type(self):
         if self.is_value(['void', 'int', 'char', 'boolean']) or self.is_name('identifier'):
-            self.eat_token()
+            self.eat_current()
             self.has_name('identifier')
-            self.eat_token()
+            self.eat_current()
 
+    # 格式必须是type varName (, varName)*;
     def compile_var_dec(self):
-        return
+        # 格式必须是type varName
+        self.compile_type()
+        # 格式必须是 (, varName)*;
+        while self.is_value(','):
+            self.eat(',')
+            self.has_name('identifier')
+            self.eat_current()
+        self.eat(';')
 
     def compile_term(self):
         return
@@ -163,5 +192,5 @@ class CompilationEngine:
 
     def raise_exception(self, value):
         print('翻译:{}'.format(self.get_current_token()))
-        print(value)
+        print('错误：', value)
         # raise Exception(value)
